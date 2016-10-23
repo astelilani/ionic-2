b2ef6b11fd66efac1e51045ead8022f9fbcf8373
astelilani <elilani@astcorporation.com>
2016-10-22 19:25:37 -0500

    wip - implementing basic login using ionic cloud

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
-import {Auth, User, UserDetails, IDetailedError} from '@ionic/cloud-angular';
+import { Auth, User, UserDetails, IDetailedError } from '@ionic/cloud-angular';
+import {TabsPage} from '../pages/tabs/tabs';
+
+let details: UserDetails = { 'email': 'hi@ionic.io', 'password': 'puppies123' };
 
 @Component({
   selector: 'page-login',
@@ -15,4 +18,27 @@ export class LoginPage {
 
   }
 
+  signup() {
+    this.auth.signup(details).then(() => {
+      // `this.user` is now registered
+    }, (err: IDetailedError<string[]>) => {
+      for (let e of err.details) {
+        if (e === 'conflict_email') {
+          alert('Email already exists.');
+        } else {
+          // handle other errors
+        }
+      }
+    });
+  }
+
+  login(){
+    this.auth.login('basic', details).then(() => {
+      this.navCtrl.push(TabsPage);
+    });
+  }
+
+  logout(){
+    this.auth.logout();
+  }
 }
